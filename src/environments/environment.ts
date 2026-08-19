213286472c5c577315e6ef32b6a02b5e0b450ab7
Sanjay Roy <s.roy@wir-machen-druck.de>
2026-08-19 16:48:08 +0530

    adding new working repository

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://societymanagement-api-f8cqfjajdkh4dpey.centralindia-01.azurewebsites.net/api',
+  hubUrl: 'https://societymanagement-api-f8cqfjajdkh4dpey.centralindia-01.azurewebsites.net/hubs/notifications'
+};
